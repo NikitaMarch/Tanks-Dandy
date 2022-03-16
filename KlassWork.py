07c2bfc8b8cab6fbcba80fed809f6fbcc3dca391
NikitaMarch <97293616+NikitaMarch@users.noreply.github.com>
2022-03-16 18:17:48 +0300

    Update KlassWork.py

--- KlassWork.py
+++ KlassWork.py
@@ -1,10 +1,13 @@
 from pygame import *
 
+bullets: list = []
+frames: int = 0
+
 
 class Tank:
     """Наивысший класс, создаёт переменные для любого танка"""
 
-    def __init__(self, noise: str, tank_x: int, tank_y, skin, speed, armor=100, health_point=100, bullets=60, width=12,
+    def __init__(self, noise: str, tank_x: int, tank_y: int, skin, speed, armor=100, health_point=100, bullets=60, width=12,
                  height=10, target='r'):
         self.noise = noise  # выводит слово сохраненное в нем
         self.skin = skin  # цвет танка
@@ -38,7 +41,9 @@ class Tank:
 
 class Buldog(Tank):
     """Наш танк"""
+
     def move(self):
+        """Двигаем наш танк с помощью клавиш"""
         if key.get_pressed()[key.key_code("w")]:
             self.tank_y -= self.speed
             self.target = 'u'  # смена направления движения наверх (up)
@@ -52,13 +57,24 @@ class Buldog(Tank):
             self.tank_x += self.speed
             self.target = 'r'
 
+    def shoot(self):
+        global bullets, frames
+        frames += 1
+        if key.get_pressed()[K_SPACE] and frames > 200\
+                :  # установить ЛКМ
+            bullet = Bullet(27, 17, 5, 6, 77, 55, 'r')
+            bullets.append(bullet)
+            frames = 0
+
 
 class Enemy(Tank):  # вражеский танк
     pass
 
 
 class Bullet:
-    """старший класс пули, общие настройки"""
+    """старший класс пули, общие настройки \n
+    :parameter target направление движения пули: 'r' - right, 'l' - left, 'u' - up или 'd' - down"""
+
     def __init__(self, damage: int, speed: int, width: int, height: int, bullet_x: int, bullet_y: int, target: str):
         self.target = target
         self.damage = damage
@@ -69,17 +85,32 @@ class Bullet:
         self.bullet_y = bullet_y
 
     def draw_bullet(self):
-        draw.rect(screen, self.width, self.height)  # Rect(1, 1, 2, 2) - пример
+        draw.rect(screen, "black", Rect(self.bullet_x, self.bullet_y, self.width, self.height))
 
     def move(self):
         """Пуля двигается (летит)"""
-        # 1) сохранять направление для танка - сделано
-        # 2) проверять направление танка (конкретно пушки) - сделано
         # 3) передать последнее направление танка (в момент выстрела) для пули
-        # 4) меняем координаты пули (двигаем) согласно этому направлению - сделано 1/4
-        # 5) если пуля вышла за пределы или пуля попала в чужой танк - пуля уничтожается - использовать del
         if self.target == 'u':
             self.bullet_y += 1
+        if self.target == 'd':
+            self.bullet_y += 1
+        if self.target == 'l':
+            self.bullet_y += 1
+        if self.target == 'r':
+            self.bullet_y += 1
+
+    @staticmethod
+    def delete_bullet():
+        global bullets
+        for bullet in bullets:
+            if bullet.bullet_y <= 0:
+                bullets.remove(bullet)
+            if bullet.bullet_x <= 0:
+                bullets.remove(bullet)
+            if bullet.bullet_x >= 800:
+                bullets.remove(bullet)
+            if bullet.bullet_y >= 600:
+                bullets.remove(bullet)
 
     def make_damage(self):
         pass
@@ -91,10 +122,12 @@ screen = display.set_mode([800, 600])
 def check_tanks_coordinates(buldog: Buldog, enemy: Enemy) -> None:
     """не разрешает танкам заезжать друг на друга"""
     if (buldog.tank_y + buldog.height > enemy.tank_y) and (buldog.tank_y < enemy.tank_y + enemy.height) \
-            and (buldog.tank_x + buldog.width >= enemy.tank_x) and (buldog.tank_x <= enemy.tank_x):  # слева направо
+            and (buldog.tank_x + buldog.width >= enemy.tank_x) and (buldog.tank_x <= enemy.tank_x):
+        # слева направо
         buldog.tank_x -= buldog.speed
     if (buldog.tank_y + buldog.height > enemy.tank_y) and (buldog.tank_y < enemy.tank_y + enemy.height) \
-            and (buldog.tank_x <= enemy.tank_x + enemy.width) and (buldog.tank_x >= enemy.tank_x):  # справа налево
+            and (buldog.tank_x <= enemy.tank_x + enemy.width) and (buldog.tank_x >= enemy.tank_x):
+        # справа налево
         buldog.tank_x += buldog.speed
 
     if (buldog.tank_y + buldog.height + 1 > enemy.tank_y) and (buldog.tank_y < enemy.tank_y + enemy.height) \
@@ -111,6 +144,8 @@ def check_tanks_coordinates(buldog: Buldog, enemy: Enemy) -> None:
 def game(play, tanks, own_tank):
     """Следит, что происходит в игре"""
 
+    global bullets  # на пятницу
+
     while play:  # бесконечный цикл, который перезапускает проверку игры
         for e in event.get():
             if e.type == QUIT:
@@ -118,11 +153,18 @@ def game(play, tanks, own_tank):
         draw.rect(screen, "white", Rect(0, 0, 800, 600))
         own_tank.check_coordinates()
 
+        # тут будем рисовать пули
+        for bullet in bullets:
+            bullet.draw_bullet()
+            bullet.move()
+
         for tank in tanks:
             check_tanks_coordinates(own_tank, tank)
             tank.draw_tank()
 
         own_tank.move()
+        own_tank.shoot()
+        Bullet.delete_bullet()
         own_tank.draw_tank()
         display.update()
 
